e401f2d89b78f0e50edb2b957ef0df89b1b1def3
Basit ali <basitalyshah51214@gmail.com>
2025-09-16 05:28:13 -0700

    work on decorocation widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firstapp/views/decorcation_widget_screen.dart';
 import 'package:firstapp/views/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashScreen(),
+      home: DecorcationWidgetScreen(),
     );
   }
 }
---
+++ lib/views/decorcation_widget_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class DecorcationWidgetScreen extends StatelessWidget {
+  const DecorcationWidgetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Card(
+            elevation: 10,
+            shadowColor: Colors.purpleAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Text("This is card widget"),
+                  ElevatedButton(onPressed: () {}, child: Text("Click Me")),
+                ],
+              ),
+            ),
+            color: Colors.amberAccent,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 60, right: 60),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                color: Colors.purple,
+                child: ListTile(
+                  leading: Icon(Icons.account_circle),
+                  title: Text("Ali Akbar"),
+                  subtitle: Text("Hello, how are you?"),
+                  trailing: Text(DateTime.now().toString()),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.all(12),
+            height: 70,
+            width: 420,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(radius: 25, backgroundColor: Colors.white),
+                    const SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Ali Akbar"),
+                        Text("Hello, how are you?"),
+                      ],
+                    ),
+                  ],
+                ),
+                Text(DateTime.now().toString()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
